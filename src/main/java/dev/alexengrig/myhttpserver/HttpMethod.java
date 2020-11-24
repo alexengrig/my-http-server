d1aa30543095a4ecef70d57b581b70cbb46fafd4
Grig Alex <alexengrigdev@gmail.com>
2020-11-25 00:29:22 +0300

    Add HttpMethod#of

--- src/main/java/dev/alexengrig/myhttpserver/HttpMethod.java
+++ src/main/java/dev/alexengrig/myhttpserver/HttpMethod.java
@@ -2,5 +2,14 @@ package dev.alexengrig.myhttpserver;
 
 public enum HttpMethod {
     GET,
-    POST
+    POST;
+
+    public static HttpMethod of(String method) {
+        for (HttpMethod httpMethod : values()) {
+            if (httpMethod.name().equalsIgnoreCase(method)) {
+                return httpMethod;
+            }
+        }
+        throw new IllegalArgumentException("Unknown HTTP method: " + method);
+    }
 }
