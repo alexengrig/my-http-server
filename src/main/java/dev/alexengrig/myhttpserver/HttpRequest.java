bd5f9a0458eba2de7768cc650ca2a3a33d78c3da
Grig Alex <alexengrigdev@gmail.com>
2020-11-25 00:30:13 +0300

    Refactor HttpRequest

--- src/main/java/dev/alexengrig/myhttpserver/HttpRequest.java
+++ src/main/java/dev/alexengrig/myhttpserver/HttpRequest.java
@@ -2,9 +2,21 @@ package dev.alexengrig.myhttpserver;
 
 public class HttpRequest {
     private final String message;
+    private final HttpMethod method;
+    private final String url;
+    private final HttpVersion version;
+    private final HttpHeaders headers;
+    private final HttpBody body;
 
-    public HttpRequest(CharSequence message) {
+    public HttpRequest(CharSequence message, HttpMethod method,
+                       String url, HttpVersion version,
+                       HttpHeaders headers, HttpBody body) {
         this.message = message.toString();
+        this.method = method;
+        this.url = url;
+        this.version = version;
+        this.headers = headers;
+        this.body = body;
     }
 
     @Override
