5ae2880b4f6771df90706c7b800a8f887879d808
Grig Alex <alexengrigdev@gmail.com>
2020-11-27 01:39:09 +0300

    Add handlers to Server

--- src/main/java/dev/alexengrig/myhttpserver/Runner.java
+++ src/main/java/dev/alexengrig/myhttpserver/Runner.java
@@ -2,9 +2,11 @@ package dev.alexengrig.myhttpserver;
 
 public class Runner {
     public static void main(String[] args) throws Exception {
-        new Server("127.0.0.1", 8008, (req, res) -> {
+        Server server = new Server("127.0.0.1", 8008);
+        server.get("/", (req, res) -> {
             HttpBody body = new HttpBody("<html><body><h1>Your request:</h1><pre>" + req + "</pre></body></html>");
             res.setBody(body);
-        }).bootstrap();
+        });
+        server.bootstrap();
     }
 }
--- src/main/java/dev/alexengrig/myhttpserver/Server.java
+++ src/main/java/dev/alexengrig/myhttpserver/Server.java
@@ -7,26 +7,35 @@ import java.nio.CharBuffer;
 import java.nio.channels.AsynchronousServerSocketChannel;
 import java.nio.channels.AsynchronousSocketChannel;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Server {
     private final int bufferSize;
     private final HttpRequestFactory requestFactory;
+    private final Map<HttpMethod, Map<String, HttpHandler>> handlers;
 
     private final String host;
     private final int port;
-    private final HttpHandler handler;
 
-    public Server(String host, int port, HttpHandler handler) {
+    public Server(String host, int port) {
         this.bufferSize = 256;
         this.requestFactory = new HttpRequestFactory();
+        this.handlers = new HashMap<>(HttpMethod.values().length);
         this.host = host;
         this.port = port;
-        this.handler = handler;
     }
 
-    private AsynchronousServerSocketChannel createServerChannel(String hostname, int port) throws IOException {
-        return AsynchronousServerSocketChannel.open().bind(new InetSocketAddress(hostname, port));
+    public void get(String url, HttpHandler handler) {
+        Map<String, HttpHandler> handlerByUrl;
+        if (handlers.containsKey(HttpMethod.GET)) {
+            handlerByUrl = handlers.get(HttpMethod.GET);
+        } else {
+            handlerByUrl = new HashMap<>();
+            handlers.put(HttpMethod.GET, handlerByUrl);
+        }
+        handlerByUrl.put(url, handler);
     }
 
     public void bootstrap() throws Exception {
@@ -40,24 +49,38 @@ public class Server {
                 continue;
             }
             HttpRequest request = getRequest(client);
-            System.out.println(request.getMethod() + " " + request.getUrl());
+            HttpMethod method = request.getMethod();
+            String url = request.getUrl();
+            System.out.println(method + " " + url);
             HttpResponse response = getResponse();
-            if (handler != null) {
-                try {
-                    handler.handle(request, response);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
-                    response.setBody(new HttpBody("<html><body><h1>Error happens</h1></body></html>"));
+            if (handlers.containsKey(method)) {
+                Map<String, HttpHandler> handlerByUrl = handlers.get(method);
+                HttpHandler handler;
+                if (handlerByUrl.containsKey(url) && (handler = handlerByUrl.get(url)) != null) {
+                    try {
+                        handler.handle(request, response);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+                        response.setBody(new HttpBody("<html><body><h1>Error happens</h1></body></html>"));
+                    }
+                } else {
+                    response.setStatus(HttpStatus.NOT_FOUND);
+                    response.setBody(new HttpBody("<html><body><h1>Resource not found</h1><pre>" + url +
+                            "</pre></body></html>"));
                 }
             } else {
                 response.setStatus(HttpStatus.NOT_FOUND);
-                response.setBody(new HttpBody("<html><body><h1>Resource not found</h1></body></html>"));
+                response.setBody(new HttpBody("<html><body><h1>Method not supported</h1></body></html>"));
             }
             client.write(ByteBuffer.wrap(response.getBytes()));
             client.close();
             System.out.println("Client is closed");
         }
+    }
+
+    private AsynchronousServerSocketChannel createServerChannel(String hostname, int port) throws IOException {
+        return AsynchronousServerSocketChannel.open().bind(new InetSocketAddress(hostname, port));
     }
 
     private HttpRequest getRequest(AsynchronousSocketChannel readChannel) throws Exception {
